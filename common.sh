238ec8bf9c3c578e1dca938a06d4e7d9ec5059f3
Jim Park <jimp@scribd.com>
2022-11-11 15:58:22 -0800

    chore: cleanup cachedir on the top of each hour

--- common.sh
+++ common.sh
@@ -6,6 +6,27 @@ ALLREPOS_EXECUTION_DIR=$(pwd)
 export ALLREPOS_ORGNAME=${ALLREPOS_EXECUTION_DIR##*/} # This is the name of the directory that this script is run from
 
 ALLREPOS_CACHEBUSTER=$(date +%Y%m%d%H)
+mkdir -p ${ALLREPOS_MYDIR}/.cache/
+
+if [[ ! -d ${ALLREPOS_MYDIR}/.cache/${ALLREPOS_CACHEBUSTER} ]]
+then
+  echo "Creating cache directory" >&2
+  rm -rf ${ALLREPOS_MYDIR}/.cache/*
+  mkdir -p ${ALLREPOS_MYDIR}/.cache/${ALLREPOS_CACHEBUSTER}
+fi
+
 mkdir -p ${ALLREPOS_MYDIR}/.cache/$ALLREPOS_CACHEBUSTER/${ALLREPOS_ORGNAME}
 
 export ALLREPOS_CACHEPATH=${ALLREPOS_MYDIR}/.cache/$ALLREPOS_CACHEBUSTER/${ALLREPOS_ORGNAME}
+
+function change_count() {
+ git status --porcelain | wc -l
+}
+export -f change_count
+
+function print_changes() {
+  printf "\n\e[33;1mChanges in {}\n"
+  git status --porcelain
+  printf "\e[0m"
+}
+export -f print_changes
