256aac95c730b1e03ff45d82619a46fd8e0b8fdd
Jim Park <jimp@scribd.com>
2022-11-11 16:53:44 -0800

    chore: separate branches and working directories

--- common.sh
+++ common.sh
@@ -25,12 +25,21 @@ function change_count() {
 export -f change_count
 
 function print_changes() {
-  if [ $(change_count) -gt 0 ]; then
-    printf "\n\e[33;1mChanges in $(pwd):\n"
+  if [[ $(change_count) -gt 0 ]]; then
+    printf "\e[33;1m"
+    printf "Unclean working directory:\n"
     git status --porcelain
     printf "\e[0m\n"
-  else
-    change_count
   fi
 }
 export -f print_changes
+
+function print_branch() {
+  BRANCH=$(git branch --show-current)
+  if [[ "$BRANCH" != "master" ]] && [[ "$BRANCH" != "main" ]]; then
+    printf "\e[33;1m"
+    printf "Branch: ${BRANCH}"
+    printf "\e[0m\n"
+  fi
+}
+export -f print_branch
